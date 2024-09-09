494e3333a7052924d6939b364b77c8ef1ddb4f55
vsingh21 <viraj3@illinois.edu>
2024-09-08 22:49:30 -0500

    Initial Commit

---
+++ Contacts Manager/Contacts_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_ManagerApp.swift
+//  Contacts Manager
+//
+//  Created by Viraj Singh on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contacts Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Contacts Manager
+//
+//  Created by Viraj Singh on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
